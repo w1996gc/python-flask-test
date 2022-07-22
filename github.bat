30e4e8d6e4e0eaa5083bcc32a72d7685e76687da
first-demo <w15730909857aa@outlook.com>
2022-07-23 06:51:12 +0800

    update

--- github.bat
+++ github.bat
@@ -5,7 +5,8 @@ git branch -M main
 rem git remote add origin https://github.com/w1996gc/python-flask-test.git
 rem git remote set-url origin git@github.com:github/w1996gc/python-flask-test.git
 
-git remote set-url origin  https://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@github.com/w1996gc/python-flask-test.git
+rem git remote set-url origin https://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@github.com/w1996gc/python-flask-test.git
+git remote set-url origin https://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@github.com/w1996gc/python-flask-test.git
 git push -u origin main
 
 timeout 160
