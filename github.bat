bbd279bf116aac1fada7c863a7339c6e07e13211
first-demo <w15730909857aa@outlook.com>
2022-08-18 06:06:35 +0800

    Elevator related folders

--- github.bat
+++ github.bat
@@ -8,5 +8,5 @@ rem git remote set-url origin git@github.com:github/w1996gc/python-flask-test.gi
 rem git remote set-url origin https://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@github.com/w1996gc/python-flask-test.git
 git remote set-url origin https://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@github.com/w1996gc/python-flask-test.git
 git push -u origin main
-
+rem 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
 timeout 160
